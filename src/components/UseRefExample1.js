5d2befda8ebd5cad1a520ff7d3e38ad781754b88
Object-ions <moshikoatia@gmail.com>
2024-03-27 12:49:16 -0700

    Add useRef to a a form input

---
+++ src/components/UseRefExample1.js
@@ -0,0 +1,36 @@
+import { useRef } from 'react';
+
+/*
+when we change the reference value we don't re-render the component like we do with set state value
+
+When you change the value of a ref, it does not trigger a component re-render. The useRef hook is primarily used for accessing DOM elements directly, and it can also be used to store any mutable value that does not cause a re-render when updated. This is particularly useful for keeping track of values across renders without causing additional renders, unlike state updates with useState, which always cause a re-render.
+*/
+
+const UseRefExample1 = () => {
+  const inputRef = useRef();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputRef.current.value);
+    inputRef.current.value = null;
+    inputRef.current.style.border = '1px dashed red';
+    inputRef.current.focus();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        ref={inputRef}
+        type="text"
+        className="form-control mb-2"
+        id="name"
+      />
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default UseRefExample1;
