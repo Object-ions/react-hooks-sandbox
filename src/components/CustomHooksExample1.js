d539c6de8f1768731426a578ae167add6be786d7
Object-ions <moshikoatia@gmail.com>
2024-03-29 22:32:07 -0700

    Refactor (Destructure)

--- src/components/CustomHooksExample1.js
+++ src/components/CustomHooksExample1.js
@@ -1,15 +1,18 @@
 import useFetch from '../hooks/useFetch';
 
 const CustomHooksExample1 = () => {
-  const res = useFetch('https://jsonplaceholder.typicode.com/posts', {});
+  const { loading, data } = useFetch(
+    'https://jsonplaceholder.typicode.com/posts',
+    {}
+  );
 
-  if (res.loading) {
+  if (loading) {
     return <h3>Loading...</h3>;
   }
 
   return (
     <div>
-      {res.data.map((post) => (
+      {data.map((post) => (
         <h3 key={post.id}>{post.title}</h3>
       ))}
     </div>
